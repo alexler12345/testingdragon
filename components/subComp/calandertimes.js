c42e0d8a65ac3e6fcb6f177d9a01ea9d4c4b64f0
alexler12345 <156706923+alexler12345@users.noreply.github.com>
2025-01-14 01:54:07 -0700

    Update calandertimes.js

--- components/subComp/calandertimes.js
+++ components/subComp/calandertimes.js
@@ -1,11 +1,11 @@
 
 
 export var Defaulttime = {
-  Monday: { start: '08:00:00', end: '17:00:00' },
-  Tuesday: { start: '08:00:00', end: '17:00:00' },
-  Wednesday: { start: '08:00:00', end: '17:00:00' },
-  Thursday: { start: '08:00:00', end: '17:00:00' },
-  Friday: { start: '08:00:00', end: '17:00:00' },
-  Saturday: { start: null, end: null },
-  Sunday: { start: null, end: null }
+  Monday: { start: '08:00:00', end: '17:00:00', isallday: false },
+  Tuesday: { start: '08:00:00', end: '17:00:00', isallday: false  },
+  Wednesday: { start: '08:00:00', end: '17:00:00', isallday: false  },
+  Thursday: { start: '08:00:00', end: '17:00:00', isallday: false  },
+  Friday: { start: '08:00:00', end: '17:00:00', isallday: false  },
+  Saturday: { start: null, end: null, isallday: false  },
+  Sunday: { start: null, end: null, isallday: false  }
 }
