b4e8d93a4564c3963a81a19b7ae57227b7e50fa9
alexler12345 <156706923+alexler12345@users.noreply.github.com>
2025-01-16 09:10:30 -0700

    glue

--- app/page.js
+++ app/page.js
@@ -1,8 +1,10 @@
-import CookieConsent from '@/components/cookiesconsent';
+
 import Footer from '@/components/footer';
 import Title from '@/components/title';
 import NewContact from '@/components/new-contact';
 import NewTime from '@/components/new-time/time';
+import { FormCal } from "@/components/subComp/test";
+import { Defaulttime } from '@/components/subComp/calandertimes';
 
 
 export const metadata = {
@@ -10,9 +12,46 @@ export const metadata = {
   description: 'Locally owned and operated forever washing trucks, trailers and RVs',
 };
 
-export default function Home() {
+
+export default async function Home() {
+  const event = new Date()
+  event.setUTCHours(23,59,59,59);
+   event.getDate(event.setDate(event.getDate()+6))
+   const events = new Date()
+    events.setUTCHours(0,0,0,0);
+   const calendarId = ["a16c4aac33575e5eddfc40fac317fc414d26f123a733f3651cacbe268e85f3a4@group.calendar.google.com","e6e9e8c902d57bb7f9d63b62c5831292c8246381ed1c6931e063168ba924bc24@group.calendar.google.com"]; // Replace with your calendar ID
+   const baseUrl = "https://www.googleapis.com/calendar/v3/calendars";
+   const apiKey = process.env.GOOGLE_CALENDAR_API_KEY; // Use your API key from environment variables
+   var o = []
+ for (let i=0; i<2; i++){
+
+     const url = `${baseUrl}/${encodeURIComponent(calendarId[i])}/events?key=${apiKey}&orderBy=startTime&singleEvents=true&timeMin=${events.toISOString()}&timeMax=${event.toISOString()}`;
+
+//86400
+try {
+
+ const response = await fetch(url,{next: {revalidate: 5}})
+ if (response.ok) {
+  const data = await response.json()
+
+  o[i] = FormCal(data)
+  
+
+ } else {
+  console.warn('Promise resolved but HTTP status failed \n Using default time for: ',i==0?'Office hours':'Bay hours');
+  o[i] = FormCal(Defaulttime[i])
+}
+} catch {
+console.error('Promise rejected');
+}
+ 
+
+  
+ }
+ 
   return (
     <>
+
       <Title />
 
       <div
@@ -31,12 +70,9 @@ export default function Home() {
           </div>
         </div>
         <div className="flex flex-col w-full lg:w-[50%]">
-          <NewTime t={false} />
+          <NewTime {...o}/>
         </div>
       </div>
-
-      {/* Uncomment this code when ToS and PP are done (talk to Davin about HR team or some lawyers to make it) */}
-      {/*<CookieConsent />*/}
 
       <Footer />
     </>
--- components/new-time/timeTable.jsx
+++ components/new-time/timeTable.jsx
@@ -1,61 +1,187 @@
 'use client'
 import './time.css'
-import isOfficeOpen from '../subComp/isOpen'
-import { useEffect } from 'react'
-import { theDay } from '../subComp/isOpen'
+import { useState } from 'react'
+import { Defaulttime } from '../subComp/calandertimes'
 
 
-export default function Timetable () {
+
+
+// Function to check if the office is open
+
+export const formatter = new Intl.DateTimeFormat('en-US', {
+  timeZone: 'America/Denver',
+  weekday: 'long'
+})
+
+
+function convertTo12Hour(start,end) {
   
-  console.log('it looks like your looking at the  source code lucky for you all of it is on https://github.com/alexler12345/Vaneetruckwash.git')
+  if (!start || !end) {
+    return 'Closed'
+  }
+  const [hours1, minutes1, seconds1] = start.split(':').map(Number);
+  const [hours2, minutes2, seconds2] = end.split(':').map(Number);
+  const period1 = hours1 >= 12 ? 'PM' : 'AM';
+  const period2 = hours2 >= 12 ? 'PM' : 'AM';
+  const hours122 = hours2 % 12 || 12;
+  const hours121 = hours1 % 12 || 12; // Convert "0" to "12" for midnight
+  return `${hours121} ${period1}-${hours122} ${period2}`;
+}
 
-  const isOpen= isOfficeOpen() 
+
+
+
+
+function isOfficeOpen (who) {
+
+
+  var isOpen = null
+ 
   
- useEffect(() => {
+  
+  
+  var dayHours = who[formatter.format(new Date())]
 
+ if (dayHours.isallday) {
+  return isOpen = true
+ }
+
+  const serverTime = new Intl.DateTimeFormat('en-US', {
+    timeZone: 'America/Denver',
+    hour: '2-digit',
+    minute: '2-digit',
+    second: '2-digit',
+    hour12: false
+  }).format(new Date())
+  
+ 
+
+  if (serverTime >= dayHours.start && serverTime <= dayHours.end) {
+    isOpen = true //open
+  }
+  if (serverTime < dayHours.start || serverTime > dayHours.end) {
+    isOpen = false
+  }
+  return isOpen
+}
+
+
+
+export default function Timetable (p) {
+  //true = office hours //false = bay hours
+  const [Timedata, setTimedata] = useState(0)
 
  
-const t = document.getElementById(theDay)
+ 
+  const who = structuredClone(p[Timedata])
 
-  if (isOpen) {
-    t.classList.add('open')
-  } if (!isOpen){
-    t.classList.add('closed')
-  } 
 
- })
+
+  console.log(
+    'it looks like your looking at the  source code lucky for you all of it is on https://github.com/alexler12345/Vaneetruckwash.git'
+  )
+console.log(who)
+
+    
+
+  
+  const today = new Date() // Current date
+  const next7Days = []
+
+  // Loop to get the current day and the next 6 days
+  for (let i = 0; i < 7; i++) {
+    const nextDay = new Date(today) // Clone the current date
+    nextDay.setDate(today.getDate() + i) // Increment the date
+    next7Days.push(formatter.format(nextDay)) // Format and add to the array
+    console.log('data: ',next7Days[i], who[next7Days[i]])
+  }
+
+  const toggleData = () => {
+    setTimeout(3000)
+    setTimedata((prevIndex) => (prevIndex === 1 ? 0 : 1))
+  }
+
+  
+  const clockIconSize = 47
+
   return (
-    <table className='openinghours'>
+    <>
+   
+   <div className='flex justify-between items-center m-2.5 lg:mb-4 pb-2.5 lg:pb-4 border-b border-b-highlight card-header'>
+          <div tabIndex={0} className='flex border-2 focus:border-5 shadow-sm focus:border-blue-100 rounded-md transition-all hover:-translate-y-0.5 active:translate-y-0.5 duration-300 select-none ease-in-out' onClick={toggleData}>
+          <div className='media-body'>
+                    <h4
+                      
+                      className='m-0 font-sans text-[#333] text-bold text-xl md:text-2xl dark:text-text1 OpeningT'
+                    >
+                      {Timedata == 0 ? 'Office hours' : 'Bay hours'}
+                    </h4>
+                  </div>
+                  
+                    <svg
+                      className='pr-1'
+                      fill='#000000'
+                      width='32px'
+                      height='32px'
+                      viewBox='0 0 32 32'
+                      xmlns='http://www.w3.org/2000/svg'
+                    >
+                      <g id='SVGRepo_bgCarrier' strokeWidth='0'></g>
+                      <g
+                        id='SVGRepo_tracerCarrier'
+                        strokeLinecap='round'
+                        strokeLinejoin='round'
+                      ></g>
+                      <g id='SVGRepo_iconCarrier'>
+                        <path d='M16.003 18.626l7.081-7.081L25 13.46l-8.997 8.998-9.003-9 1.917-1.916z'></path>
+                      </g>
+                    </svg>
+                  
+          </div>
+                  <svg
+                    xmlns='http://www.w3.org/2000/svg'
+                    viewBox='0 0 512 512'
+                    className='dark:fill-text1 fill-[#333]'
+                    width={clockIconSize}
+                    height={clockIconSize}
+                  >
+                    <path d='M464 256A208 208 0 1 1 48 256a208 208 0 1 1 416 0zM0 256a256 256 0 1 0 512 0A256 256 0 1 0 0 256zM232 120l0 136c0 8 4 15.5 10.7 20l96 64c11 7.4 25.9 4.4 33.3-6.7s4.4-25.9-6.7-33.3L280 243.2 280 120c0-13.3-10.7-24-24-24s-24 10.7-24 24z'></path>
+                  </svg>
+                </div>
+  
+   <table className="openinghours">
       <tbody>
-        <tr id='Monday'>
-          <th>Monday</th>
-          <td className='text-right'>8 AM–5 PM</td>
-        </tr>
-        <tr id='Tuesday'>
-          <th>Tuesday</th>
-          <td className='text-right'>8 AM–5 PM</td>
-        </tr>
-        <tr id='Wednesday'>
-          <th>Wednesday</th>
-          <td className='text-right'>8 AM–5 PM</td>
-        </tr>
-        <tr id='Thursday'>
-          <th>Thursday</th>
-          <td className='text-right'>8 AM–5 PM</td>
-        </tr>
-        <tr id='Friday'>
-          <th>Friday</th>
-          <td className='text-right'>8 AM–5 PM</td>
-        </tr>
-        <tr id='Saturday'>
-          <th>Saturday</th>
-          <td className='text-right'>Closed</td>
-        </tr>
-        <tr id='Sunday'>
-          <th>Sunday</th>
-          <td className='text-right'>Closed</td>
-        </tr>
+      {[...Array(7)].map((_, index) => {
+    const dayData = who[next7Days[index]] || {}; // Use default empty object
+    return (
+      <tr
+        tabIndex={0}
+        title={Timedata == 1 ? 'Bay hours' : 'Office hours'}
+        key={index}
+        id={index.toString()}
+        className={`focus:border-3 focus:border-blue-100 ${
+          index == 0 && !isOfficeOpen(who) ? 'closed' : 'open'
+        }`}
+      >
+        <th>{next7Days[index]}</th>
+        <td
+          className={`text-right ${
+            convertTo12Hour(dayData.start, dayData.end) === 'Closed' &&
+            dayData.isallday === false
+              ? 'offDays'
+              : ''
+          }`}
+        >
+          {dayData.isallday && dayData.start == null
+            ? '24 hours'
+            : convertTo12Hour(dayData.start, dayData.end)}
+        </td>
+      </tr>
+    );
+})}
+
       </tbody>
     </table>
+    </>
   )
 }
